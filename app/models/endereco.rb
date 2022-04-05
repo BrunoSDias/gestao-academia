62a61dc42ed7e28020496eb088f082153505c5cd
Bruno Dias <brunosdias@outlook.com>
2022-04-04 21:29:03 -0300

    fix endereco model validation

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,5 +1,5 @@
 class Endereco < ApplicationRecord
   belongs_to :cliente
 
-  validates rua, cep, estado, cidade, numero, bairro, presence: true
+  validates :rua, :cep, :estado, :cidade, :numero, :bairro, presence: true
 end
