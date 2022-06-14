4f50bf56638cb426eadc1c011c603fa749beb81b
Bruno Dias <brunosdias@outlook.com>
2022-06-13 21:51:25 -0300

    added allowed origins to websocket production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = 'https://gestao-academia-client.vercel.app/'
+  config.action_cable.allowed_request_origins = 'https://gestao-academia-client.vercel.app'
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
