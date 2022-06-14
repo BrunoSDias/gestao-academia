5a6b3d1240d03be0f85e6974242b6ab4dedf1dde
Bruno Dias <brunosdias@outlook.com>
2022-06-13 21:34:01 -0300

    added front end production dns

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins ['http://localhost:3000', 'https://gestao-academia-client.vercel.app/']
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options],
